ad57c023c8ae8967e35408543d9aedc7cd8749eb
YashS75 <yashsalvi075@gmail.com>
2025-05-09 13:45:33 +0530

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -1,1 +1,27 @@
+/* File: src/shell.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "shell.h"
+#include "parser.h"
+#include "executor.h"
+
+#define MAX_LINE 1024
+
+void shell_loop() {
+    char line[MAX_LINE];
+    char **args;
+    int status = 1;
+
+    while (status) {
+        printf("mysh> ");
+        if (!fgets(line, MAX_LINE, stdin)) break;
+        args = parse_line(line);
+        if (args[0] != NULL) {
+            status = execute_command(args);
+        }
+        free(args);
+    }
+}
 
