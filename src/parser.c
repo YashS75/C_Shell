15bdfd49cb950af82abc77f9db8d23dab30f1d40
YashS75 <yashsalvi075@gmail.com>
2025-05-09 13:47:01 +0530

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,21 @@
+/* File: src/parser.c */
+#include <stdlib.h>
+#include <string.h>
+#include "parser.h"
 
+#define MAX_TOKENS 64
+#define DELIM " \\t\n"
+
+char **parse_line(char *line) {
+    char **tokens = malloc(MAX_TOKENS * sizeof(char*));
+    char *token;
+    int position = 0;
+
+    token = strtok(line, DELIM);
+    while (token != NULL) {
+        tokens[position++] = token;
+        token = strtok(NULL, DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
