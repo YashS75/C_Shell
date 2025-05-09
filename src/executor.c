7a114ec08a71c905b4d8a9f99b2f3ca119606302
YashS75 <yashsalvi075@gmail.com>
2025-05-09 13:47:41 +0530

    Update executor.c

--- src/executor.c
+++ src/executor.c
@@ -1,1 +1,24 @@
+/* File: src/executor.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include "executor.h"
+
+int execute_command(char **args) {
+    if (strcmp(args[0], "exit") == 0) return 0;
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        execvp(args[0], args);
+        perror("execvp failed");
+        exit(1);
+    } else if (pid > 0) {
+        waitpid(pid, NULL, 0);
+    } else {
+        perror("fork failed");
+    }
+    return 1;
+}
 
